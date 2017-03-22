821ede750c32a08de62006c9c6f4c46ec4ff23a8
natha <natha@LAPTOP-I21E7IVS>
2017-03-22 12:11:25 +0000

    hijn

--- Planner/src/planner/Planner.java
+++ Planner/src/planner/Planner.java
@@ -17,6 +17,7 @@ public class Planner {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hi");
+        //this is my comment
     }
     
 }

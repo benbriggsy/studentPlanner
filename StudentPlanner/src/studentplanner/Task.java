1f50f1fea81e1d05d77602bea02fe7d3b0081e8d
natha <natha@LAPTOP-I21E7IVS>
2017-04-15 19:52:06 +0100

    Added Activity, Assesmment, Task and Student  classes

---
+++ StudentPlanner/src/studentplanner/Task.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentplanner;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author natha
+ */
+public class Task {
+    
+    private String taskName;
+    private int taskID;
+    private String notes;
+    private ArrayList<Activity> activityList;
+    private Assessment assessment;
+    private double weighting;
+}

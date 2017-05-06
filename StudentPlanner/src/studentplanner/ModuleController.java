1d88c9ae0d3d12b262b32aac658e1282bbf1af4d
phillipperks <phillipperks@phillips-mbp.home>
2017-05-06 14:57:33 +0100

    ModuleController

---
+++ StudentPlanner/src/studentplanner/ModuleController.java
@@ -0,0 +1,56 @@
+package studentplanner;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author phillipperks
+ */
+public class ModuleController {
+    private DashboardController dashboard;
+    private Module module;
+    
+    public String getModuleCode(){
+       return module.getModuleCode();
+    }
+    
+    public String getModuleTitle(){
+        return module.getModuleTitle();
+    }
+    
+    public Admin getModuleOrganiser(){
+        return module.getModuleOrganiser();
+    }
+    
+    public ArrayList<Assessment> getAssessments(){
+        return module.getAssessments();
+    }
+    
+    public ArrayList<Double> getAssessmentGrades(){
+        return module.getAssessmentGrades();
+    }
+    
+    public double getCurrentGrade(){
+        return module.getCurrentGrade();
+    }
+    
+    public boolean getModuleCompleted(){
+        return module.getModuleCompleted();
+    }
+    
+    public String getNotes(){
+        return module.getNotes();
+    }
+    
+    public void addNote(String note){
+        module.addNote(note);
+    }
+    
+    public void setModuleCompleted(boolean c){
+        module.setModuleCompleted(c);
+    }
+    
+    public DashboardController getDashboardController(){
+        return dashboard;
+    }
+}

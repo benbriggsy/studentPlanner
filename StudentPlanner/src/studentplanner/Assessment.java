e5e3c293f58993c2ef3e67ce6249b9177d695873
natha <natha@LAPTOP-I21E7IVS>
2017-05-06 22:12:20 +0100

    Added methods to Student, Activity, Task and Student methods

--- StudentPlanner/src/studentplanner/Assessment.java
+++ StudentPlanner/src/studentplanner/Assessment.java
@@ -5,10 +5,35 @@
  */
 package studentplanner;
 
+import java.util.ArrayList;
 /**
  *
  * @author natha
  */
 public class Assessment {
+    private String assessmentCode;
+    private String assessmentTitle;
+    private double weighting;
+    private double grade;
+    //private Deadline deadline;
+    private ArrayList<Task> taskList;
+    private String notes;
+    //private Progress progress;
     
+//    public Assessment(String assessmentCode, String assessmentTitle, double weighting,
+//        double grade, Deadline deadline, ArrayList<Task> taskList, String notes,
+//        Progress progress){
+//        this.assessmentCode = assessmentCode;
+//        this.assessmentTitle = assessmentTitle;
+//        this.weighting = weighting;
+//        this.grade = grade;
+//        this.deadline = deadline;
+//        this.taskList = taskList;
+//        this.notes = notes;
+//        this.progress = progress;
+//    }
+    
+    public String getAssessmentCode(){
+        return assessmentCode;
+    }
 }

28691ca46e9bb2f90288888eae32a5c5124cd422
michael meyne <michael@meyne.co.uk>
2017-05-07 11:53:46 +0100

    Added Deadline, Assignment, and a few variables in Assessment

--- StudentPlanner/src/studentplanner/Assessment.java
+++ StudentPlanner/src/studentplanner/Assessment.java
@@ -5,10 +5,21 @@
  */
 package studentplanner;
 
+import java.util.*;
+
 /**
  *
  * @author natha
  */
-public class Assessment {
+public abstract class Assessment {
     
+    //Added these so I could properly build the assignment constructor
+    
+    String assessmentCode;
+    String assessmentTitle;
+    Double weighting;
+    Double grade;
+    Deadline deadline;
+    ArrayList<Task> taskList;
+    String notes;
 }

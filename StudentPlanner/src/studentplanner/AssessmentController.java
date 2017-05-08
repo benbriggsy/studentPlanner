03ef6de528e2028336c285747042688aeb6808e7
michael meyne <michael@meyne.co.uk>
2017-05-08 15:46:47 +0100

    Added Deadline, Assignment, and a few variables in Assessment

---
+++ StudentPlanner/src/studentplanner/AssessmentController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentplanner;
+
+/**
+ *
+ * @author me
+ */
+public class AssessmentController {
+    
+    Assessment assessment;
+    
+    void ViewTask(Task task){
+        for(Task tsk: assessment.taskList){
+            if (tsk.equals(task)){
+                
+            }
+        }
+    }
+    
+    void CreateTask(){
+        
+    }
+    
+    
+    
+}
